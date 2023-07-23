e9bdbd7c260425c899581c2a48573d3ca9be7545
sophiep07 <parsonso14@craighead.school.nz>
2023-07-24 09:49:42 +1200

    renamed function name and button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let petName = ["Buddy", "Bear", "Daisy", "Coco", "Duke"]
 //the array contains the names of the pets
 
-function petNames() {
+function addPet() {
   let newPetName = prompt("Add a new pet: What is the name of the pet?")
   petName.push(newPetName)
   //asks the user for the name of a new pet and code adds it to the array
