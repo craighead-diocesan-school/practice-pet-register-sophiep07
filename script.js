995cbbcce1a3c6b90936408ebfab9ce0639dcd6f
sophiep07 <parsonso14@craighead.school.nz>
2023-07-24 09:44:59 +1200

    started, created an array of names and a function to add a new name which then adds it to the array

---
+++ script.js
@@ -0,0 +1,8 @@
+let petName = ["Buddy", "Bear", "Daisy", "Coco", "Duke"]
+//the array contains the names of the pets
+
+function petNames() {
+  let newPetName = prompt("Add a new pet: What is the name of the pet?")
+  petName.push(newPetName)
+  //asks the user for the name of a new pet and code adds it to the array
+}
