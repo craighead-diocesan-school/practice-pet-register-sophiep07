74c92b2f35563cac60c4e64e2d85a79cccb4e976
sophiep07 <parsonso14@craighead.school.nz>
2023-07-27 10:49:03 +1200

    updated the buttons and added a new funciton

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function showPet() {
   alert("Here are the pets in our system!")
   for (let pet of pets) alert(pet.name + " is a " + pet.age + " year old " + pet.species + ".")
   //outputs the array of pets for the user
+  //maybe write it so it says: 'name' is a 'age' year old 'species'
 }
-//maybe write it so it says: 'name' is a 'age' year old 'species'
 
 let pets = [
-  { name: "Buddy", age: 4, species: "Dog" },
-  { name: "Coco", age: 7, species: "Cat" },
-  { name: "Cotton", age: 2, species: "Sheep" },
-  { name: "Duke", age: 1, species: "Fish" },
-  { name: "Buddy", age: 6, species: "Horse" },
+  { name: "Buddy", age: 4, species: "dog" },
+  { name: "Coco", age: 7, species: "cat" },
+  { name: "Cotton", age: 2, species: "sheep" },
+  { name: "Duke", age: 1, species: "fish" },
+  { name: "Buddy", age: 6, species: "horse" },
 ]
 //the array contains the names, age, and species of the pets
-//they used to be just strings but now they are changed to objectfs
+//they used to be just strings but now they are changed to objects
 
 function addPet() {
   alert("Add a new pet to the system! Enter its details here.")
@@ -29,3 +29,5 @@ function addPet() {
   //asks the user for the name, age, and species of a new pet
   //then it makes a new object which is then pushed to the array of pets
 }
+
+function searchPet() {}
